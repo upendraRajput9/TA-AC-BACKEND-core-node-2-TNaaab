9c9160bbb385eebf2f7d035a7f1727a9aefba5a3
upendra <upendrarajput9911@gmail.com>
2022-03-14 04:38:50 +0530

    done

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,37 +1,23 @@
-// writeCode
+var http = require('http')
+var server = http.createServer(handleRequest)
+var qs = require('querystring')
 
-// 1. Create a basic server `data.js`
-
-// - add a listener on port 7000
-// - send different types of data from postman and
-//  check `req.headers` for `content-type` after creating below server
-// - send json data from postman using `POST` request on `/json` and
-//  parse it into the server.
-// - send form data from postman using `POST` request on `/form` and
-//  parse it into the server.
-// - send in response the entire data received by server.
-
-
-var http = require('http');
-var server = http.createServer(handleRequest);
-var qs = require('querystring');
-function handleRequest(req,res){
-let store = "";
-req.on('data',(chunk)=>{
-    store= store+chunk
-})
-req.on('end',()=>{
-    if(req.method==='POST',req.url==='/json'){
-var parseData = JSON.parse(store);
-res.end(parseData);
+function handleRequest(req, res) {
+  let store = ''
+  req.on('data', (chunk) => {
+    store = store + chunk
+  })
+  req.on('end', () => {
+    if ((req.method === 'POST', req.url === '/json')) {
+      setImmediate.header('Content-Type', 'type/json')
+      res.end(store)
     }
-    else if(req.method==='POST',req.url==='/form'){
-    var parseData = qs.parse(store);
-    res.end(JSON.stringify(parseData))
+    if ((req.method === 'POST', req.url === '/form')) {
+      var parseData = qs.parse(store)
+      res.end(JSON.stringify(parseData))
     }
-})
-
+  })
 }
-server.listen(7000,()=>{
-    console.log('server listening on port 7k')
+server.listen(7000, () => {
+  console.log('server listening on port 7k')
 })
