e2df228a2a3507536976150f2109a924e556aef0
upendra <upendrarajput9911@gmail.com>
2022-03-14 01:58:30 +0530

    done

--- block-BNaabb/stream.js
+++ block-BNaabb/stream.js
@@ -1,4 +1,3 @@
-
 // Create a basic http server in `stream.js`
 
 // - add a listener on port 3456
@@ -7,26 +6,23 @@
 // - capture the data sent from postman on server side
 // - send captured data in response using `res.write`
 
-var http = require('http');
+var http = require('http')
 
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest)
 
-function handleRequest(req,res){
- let store = "";
- if(req.method==='POST' && req.url==='/'){
-     req.on('data',(chunk)=>{
-         store=store+chunk
-     })
+function handleRequest(req, res) {
+  let store = ''
 
-     req.on('end',()=>{
-        console.log(store)
-    })
+  req.on('data', (chunk) => {
+    store = store + chunk
+  })
+
+  req.on('end', () => {
     res.write(store)
- }
-
- 
+    res.end()
+  })
 }
 
-server.listen(3456,()=>{
-    console.log('listener on port 3456')
+server.listen(3456, () => {
+  console.log('listener on port 3456')
 })
